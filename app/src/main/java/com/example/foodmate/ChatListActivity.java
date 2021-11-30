29b1ecd5409342fd03cd6c388d80054462f07122
JungDae <cj9745@naver.com>
2021-12-01 05:16:01 +0900

    Add Party DB
    채팅 리스트에서 방을 만들면 자동으로 해당 버튼을 누른 유저를 파티장으로 하는 파티 DB가 새롭게 생성되도록 기능 추가

--- app/src/main/java/com/example/foodmate/ChatListActivity.java
+++ app/src/main/java/com/example/foodmate/ChatListActivity.java
@@ -1,18 +1,40 @@
 package com.example.foodmate;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ChatListActivity extends AppCompatActivity {
+
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); //현재 접속 중인 유저 인스턴스 get
+    private FirebaseFirestore db; //데이터베이스 인스턴스 get
+    private Map<String, Object> party = new HashMap<>(); //데이터베이스 임시 저장 맵
+
+    static String partyName;
 
     private RecyclerAdapter adapter;
 
@@ -21,14 +43,40 @@ public class ChatListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_list);
 
-        init();
+        db = FirebaseFirestore.getInstance(); // db 인스턴스 get
 
+        init();
         getData();
 
         Button make_room = (Button) findViewById(R.id.make_chat);
         make_room.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final String uid = user.getUid(); //현재 접속 중인 유저 uid 추출
+                Toast.makeText(ChatListActivity.this, uid, Toast.LENGTH_LONG).show();
+
+                //앞에서 추출한 uid를 기반으로 user DB에서 조회한 뒤 현재 로그인 한 유저의 이름 추출
+                db.collection("user").whereEqualTo("uid", uid).get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if(task.isSuccessful()){
+                                    for(QueryDocumentSnapshot document : task.getResult()){
+                                        partyName = (String)document.getData().get("name") + "'s Party";
+
+                                        //파티 맵에 정보 저장
+                                        party.put("uid", uid);
+                                        party.put("partyName", partyName);
+                                        party.put("category", "");
+
+                                        //party db set
+                                        db.collection("party").add(party);
+                                        Toast.makeText(ChatListActivity.this, "파티 DB 생성 완료", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            }
+                        });
+
                 Intent intent = new Intent(getApplicationContext(), temp_chat.class);
                 startActivity(intent);
             }
